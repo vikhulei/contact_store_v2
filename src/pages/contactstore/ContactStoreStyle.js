d7979b188e13175d8dc17dc6a17c4d14964d1560
vikhulei <vikhulei@gmail.com>
2022-08-22 18:24:49 +0100

    contactstore margins and paddings fixed

--- src/pages/contactstore/ContactStoreStyle.js
+++ src/pages/contactstore/ContactStoreStyle.js
@@ -2,29 +2,25 @@ import styled from "styled-components"
 
 const Wrapper = styled.div`
     position: absolute;
-    top: 150px;
+    top: 20vh;
     left: 50%;
     transform: translateX(-50%);
     width: 90%;
-    // min-width: 1300px;
-    // height: 100%;
     display: flex;
     justify-content: space-between;
     align-items: center;
-    // background-color: lightblue;
     @media screen and (max-width: 1450px) {
         flex-direction: column-reverse;
         height: 1500px;
+        padding-bottom: 10%;
     }
     @media screen and (max-width: 1050px) {
-        top: 100px;
-        height: 2050px;
+        top: 5vh;
+        height: 2150px;
     }
 `
 
-const ProfileWrapper = styled.div `
-
-`
+const ProfileWrapper = styled.div ``
 
 const ContactsWrapper = styled.div`
 width: 100%;
@@ -32,7 +28,6 @@ display: flex;
 justify-content: space-evenly;
 align-items: center;
 @media screen and (max-width: 1050px) {
-    // background-color: blue;
     flex-direction: column;
     height: 1500px;
 }
